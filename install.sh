788c9a301e5653f38e5d724133534128df948d66
rubenmarques98 <a21270652@isec.pt>
2024-02-09 21:46:57 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,18 +2,19 @@ set -e -x
 
 clear
 
-sudo rm -rf ~/*
+sudo rm -rf /root/*
+sudo rm -rf /home/userland/*
 
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install git -y
 sudo apt-get install dnsutils -y
 
-sudo git clone -f https://github.com/rubenmarques98/ozzo-vpn.git ~
+sudo git clone -f https://github.com/rubenmarques98/ozzo-vpn.git /root
 
-sudo chmod 777 -R ~/
+sudo chmod 777 -R /root
 
-sudo sh ~/configure.sh
+sudo sh /root/configure.sh
 
 sudo cp -r /root/* /home/userland/
 
