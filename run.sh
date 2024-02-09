e56291146db286814a25272105cf000718187e0b
rubenmarques98 <a21270652@isec.pt>
2024-02-09 21:36:06 +0100

    asdfadf

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ ipv4=$(dig +short $hostname | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$' | head 
 echo The public IP of the VPN is $ipv4
 echo Starting the VPN bridge... The wall is going down!
 
-wg-tcp-tunnel --src-udp=0.0.0.0:51869 --dst-tcp=$ipv4:$1
+wg-tcp-tunnel --src-udp=0.0.0.0:51869 --dst-tcp=$ipv4:443
