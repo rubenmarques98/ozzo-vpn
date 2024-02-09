ca41daa7b47e36e9a29b0c2c2c4436e546c5e131
rubenmarques98 <a21270652@isec.pt>
2024-02-09 20:42:05 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install git -y
 
-git clone https://github.com/rubenmarques98/ozzo-vpn.git
-
-sudo cp -r ~/ozzo-vpn/* ~/
+git clone https://github.com/rubenmarques98/ozzo-vpn.git .
 
 sudo chmod 777 -R .
 
