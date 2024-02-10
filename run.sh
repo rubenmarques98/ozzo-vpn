a82676aa4ca065b535fe2516d0548d3d5a679270
rubenmarques98 <a21270652@isec.pt>
2024-02-10 12:02:29 +0100

    saf

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ ipv4=$(dig +short $hostname | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$' | head 
 
 echo The public IP of the VPN is $ipv4
 echo Starting the VPN bridge... The wall is going down!
+echo "If there is a problem, restart the application (or press Ctrl + C) and type ./run.sh again."
 
 # wg-tcp-tunnel --src-udp=0.0.0.0:51869 --dst-tcp=$ipv4:443
 
--- run_python.py
+++ run_python.py
@@ -1,5 +1,7 @@
 import subprocess
 import socket
+import select
+import time
 
 def resolve_dns(hostname):
     try:
@@ -10,29 +12,33 @@ def resolve_dns(hostname):
         return None
 
 def run_command(ipv4):
-    print("Launching script...")
     command = f"wg-tcp-tunnel --src-udp=0.0.0.0:51869 --dst-tcp={ipv4}:443"
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     return process
 
 def main():
-    hostname = "casa-resgatados.dynip.sapo.pt"  # Change this to your DNS hostname
+    hostname = "example.com"  # Change this to your DNS hostname
     ipv4 = resolve_dns(hostname)
     if ipv4:
         process = run_command(ipv4)
+        last_positive_message_time = time.time()
         # Monitor the command's output
         while True:
-            output = process.stderr.readline().strip()
-            if output:
-                print(output)
-                if "broken pipe" in output.lower():
-                    print("!!!!! Restarting script due to broken pipe...")
-                    process.terminate()  # Terminate the command process
-                    process = run_command(ipv4)  # Restart the command
-                else:
-                    print("The VPN is ok for now...")
+            # Check if there's any data available to read
+            ready = select.select([process.stderr], [], [], 0.1)[0]
+            if ready:
+                output = process.stderr.readline().strip()
+                if output:
+                    print(output)
+                    if "broken pipe" in output.lower():
+                        print("Restarting script due to broken pipe...")
+                        process.terminate()  # Terminate the command process
+                        process = run_command(ipv4)  # Restart the command
             else:
-                break  # Break the loop when no more output
+                # Print a positive message every 10 seconds
+                if time.time() - last_positive_message_time >= 30:
+                    print("The VPN is working fine.")
+                    last_positive_message_time = time.time()
     else:
         print("Failed to resolve DNS. Exiting.")
 
