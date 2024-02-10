c638baa3d6d3a7673b9f6a7438dc78de67090940
rubenmarques98 <a21270652@isec.pt>
2024-02-10 12:18:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install git -y
 sudo apt-get install dnsutils -y
-sudo apt-get install python3 python-is-python3
+sudo apt-get install python3 python-is-python3 -y
 
 sudo git clone https://github.com/rubenmarques98/ozzo-vpn.git
 
