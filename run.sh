e17e3221920f61a4037a275efa8fe00068c2ecf4
rubenmarques98 <a21270652@isec.pt>
2024-02-10 12:03:45 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
-hostname="casa-resgatados.dynip.sapo.pt"
+clear
 
-ipv4=$(dig +short $hostname | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$' | head -n 1)
+#hostname="casa-resgatados.dynip.sapo.pt"
+
+#ipv4=$(dig +short $hostname | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$' | head -n 1)
 
 echo The public IP of the VPN is $ipv4
 echo Starting the VPN bridge... The wall is going down!
-echo "If there is a problem, restart the application (or press Ctrl + C) and type ./run.sh again."
+echo "If there is a problem, restart the Userland application (or press Ctrl + C) and type ./run.sh again."
+
 
 # wg-tcp-tunnel --src-udp=0.0.0.0:51869 --dst-tcp=$ipv4:443
 
