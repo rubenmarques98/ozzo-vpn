8ad190889f717f826731e50eaaddd565cfd8186d
rubenmarques98 <56482064+rubenmarques98@users.noreply.github.com>
2024-02-11 10:14:29 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,6 +8,9 @@ echo The public IP of the VPN is $ipv4
 echo Starting the VPN bridge... The wall is going down!
 echo "If there is a problem, restart the Userland application (or press Ctrl + C) and type ./run.sh again."
 
+echo Your IPs are listed here:
+echo
+ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*'
 
 # wg-tcp-tunnel --src-udp=0.0.0.0:51869 --dst-tcp=$ipv4:443
 
